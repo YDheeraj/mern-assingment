fa762364ff5fd7044beabe522d760b6ed5d905ec
Dheeraj Kumar Yadav <90107700+YDheeraj@users.noreply.github.com>
2023-05-08 18:20:14 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,20 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 var cookieParser = require('cookie-parser')
 app.use(cookieParser())
+const path=require("path");
 
 dotenv.config({path:'./config.env'});
 require('./db/conn');
 
 app.use(express.json());
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+    });
 
 
 // linking router files
