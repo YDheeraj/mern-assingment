8e33c4bd9dd5f0ca0b8acaae29af70f82fad48a7
Dheeraj Kumar Yadav <yadavdheeraj2002@gmail.com>
2023-05-08 17:57:49 +0530

    changes

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,17 @@ app.use(express.json());
 // linking router files
 app.use(require('./router/auth'));
 
- const PORT = process.env.PORT;
+const PORT = process.env.PORT||5000;
+
+ app.use(express.static(path.join(__dirname,"./frontend/build")));
+app.get("*",function(_,res){
+    res.sendFile(
+        path.join(__dirname,"./frontend/build/index.html"),
+        function(err){
+            res.status(500).send(err);
+        }
+    );
+});
 
 app.listen(PORT,()=>{
     console.log(`listening at port no ${PORT}`)
